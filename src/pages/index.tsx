dd056d1a9f79e8b4b252bf43ea5d0cdaab02f845
tgaye <tylercgaye@gmail.com>
2024-03-23 05:11:07 -0600

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -375,7 +375,7 @@ const handleWhitelistMintClick = async () => {
          </div>
 
           <div style={{ textAlign: 'center', margin: '20px', fontSize: '4vh', color: 'white' }}>
-            <span>Total NFTs Minted: {1003}</span>
+            <span>Total NFTs Minted: {1019}</span>
           </div>
 		 {claimRewardsClicked && (
 			<div style={{ textAlign: 'center' }}>
